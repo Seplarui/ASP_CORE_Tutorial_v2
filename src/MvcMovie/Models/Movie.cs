6d4d767c7fcfaf618714268b911b1983f4d87fc7
Sebas Sebas <seplarui@gmail.com>
2016-11-03 22:58:46 +0100

    Modelo Movie

--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -7,5 +7,16 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
+
+        public int ID { get; set; }
+
+        public string Tittle { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
     }
 }
